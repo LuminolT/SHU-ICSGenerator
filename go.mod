f3ab5423c1db8cff105764a0c8453f395c72702a
LuminolT <copper_sulfate@qq.com>
2021-08-26 22:03:16 +0800

    Add Function: Read Table

---
+++ go.mod
@@ -0,0 +1,15 @@
+module main
+
+go 1.17
+
+require (
+	github.com/arran4/golang-ical v0.0.0-20210825232153-efac1f4cb8ac // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.3 // indirect
+	github.com/richardlehane/msoleps v1.0.1 // indirect
+	github.com/xuri/efp v0.0.0-20210322160811-ab561f5b45e3 // indirect
+	github.com/xuri/excelize/v2 v2.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/xuri/excelize/v2"
+)
+
+type course struct {
+	courseName string
+	courseTime string
+	courseRoom string
+}
+
+func main() {
+	courseList, err := readTable("course_table.xlsx", "Sheet1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(courseList)
+}
+
+func (this *course) timeSet() error {
+
+}
+
+func readTable(fileName, sheetName string) ([]course, error) {
+	//ReadCourse
+	courseList := make([]course, 0)
+	xlFile, err := excelize.OpenFile(fileName)
+	if err != nil {
+		return courseList, err
+	}
+	rows, err := xlFile.GetRows(sheetName)
+	if err != nil {
+		return courseList, err
+	}
+	for rowIdx, row := range rows {
+		tempRunes := []rune(row[0]) //Row Front Slice
+		if rowIdx == 0 {
+			continue
+		}
+		if tempRunes[0] > 'Z' {
+			break
+		}
+		tempCourse := course{row[2], row[6], row[7]}
+		courseList = append(courseList, tempCourse)
+	}
+	return courseList, err
 }
