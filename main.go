45b94199406b8d5bb547a627ee4e56d38a912cfa
LuminolT <copper_sulfate@qq.com>
2021-08-27 13:30:31 +0800

    Add Logistic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -15,18 +16,18 @@ type time struct {
 
 type course struct {
 	name string
-	time []time
 	room string
+	time
 }
 
-func courseSet(nameInfo, timeInfo, roomInfo string) ([]course, error) {
-	var temp course
-	courseList := make([]course, 0)
-	temp.name = nameInfo
-	temp.room = roomInfo
+// func courseSet(nameInfo, timeInfo, roomInfo string) ([]course, error) {
+// 	var temp course
+// 	courseList := make([]course, 0)
+// 	temp.name = nameInfo
+// 	temp.room = roomInfo
 
-	return courseList
-}
+// 	return courseList
+// }
 
 func main() {
 	courseList, err := readTable("course_table.xlsx", "Sheet1")
@@ -56,15 +57,51 @@ func readTable(fileName, sheetName string) ([]course, error) {
 			break
 		}
 		//每次读到时间，要先进行切片处理
-		tempCourseList, err := courseSet(row[2], row[6], row[7])
-		if err != nil {
-			return courseList, err
+		// tempCourseList, err := courseSet(row[2], row[6], row[7])
+		// if err != nil {
+		// 	return courseList, err
+		// }
+		// row[6] for timeInfo
+		var tempCourse course
+		tempCourse.name = row[2]
+		tempCourse.room = row[7]
+		tempTimeList := timeHandle(row[6])
+		for _, tempTime := range tempTimeList {
+			tempCourse.time = tempTime
+			courseList = append(courseList, tempCourse)
 		}
-		for course := range tempCourseList
-		courseList = append(courseList, tempCourse)
 	}
 	return courseList, err
 }
 
 // Todo:
 // 正确流程：先确定时间，获得一个TimeList，然后time := range TimeList进行遍历，把CourseName和CourseRoom传进去，一个个加到courseList
+
+func timeHandle(timeInfo string) []time, error {
+	timeList := make([]time, 0)
+	//第一遍，筛里面有没有周这个字，两种情况，1-5周，6-10周这种，1,6周，2,7周这种
+	//第二遍，做切分，切分的时候根据里面有没有单双周进行一个判断
+	timeInfoSlice, err := strings.Split(timeInfo, " ")
+	if err != nil {
+		return timeList, err
+	}
+	for _, timePiece := range timeInfoSlice {
+		if 这里有单双周() {
+			if 判断单双周() {
+				单周设为true()
+			} else {
+				双周设为true()
+			}
+			把单双周去掉()
+		} else if 这里有周() {
+			if 正则判断新研还是行政() {
+				把前5周后5周设为true()
+			} else {
+				把对应周设为true()
+			}
+		} else {
+			全部设为true()
+		}
+	}
+	return timeList, err
+}
